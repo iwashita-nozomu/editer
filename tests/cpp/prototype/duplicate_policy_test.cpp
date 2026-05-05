a85c5d676e5b538479620ecb056d3a7f05e94d59
iwashita-nozomu <n.iwashita@toki.waseda.jp>
2026-05-05 22:43:07 +0900

    Add duplicate-open decision policy prototype with tests

---
+++ tests/cpp/prototype/duplicate_policy_test.cpp
@@ -0,0 +1,25 @@
+#include "editor_proto/duplicate_policy.hpp"
+
+#include <cassert>
+
+int main() {
+  using namespace editor_proto;
+
+  DuplicateResolver resolver;
+  DuplicateNotice dup_file{true, "/a", {DuplicateAction::FocusExisting, DuplicateAction::OpenNewView}};
+  auto d1 = resolver.resolve_file_notice(dup_file);
+  assert(d1.action == DuplicateAction::FocusExisting);
+  assert(!d1.remember_choice);
+
+  resolver.set_file_policy(DuplicatePolicy::OpenNewView);
+  auto d2 = resolver.resolve_file_notice(dup_file);
+  assert(d2.action == DuplicateAction::OpenNewView);
+  assert(d2.remember_choice);
+
+  DuplicateNotice dup_root{true, "/r", {DuplicateAction::FocusExisting, DuplicateAction::RegisterAlias}};
+  resolver.set_root_policy(DuplicatePolicy::RegisterAlias);
+  auto d3 = resolver.resolve_root_notice(dup_root);
+  assert(d3.action == DuplicateAction::RegisterAlias);
+
+  return 0;
+}
---
+++ tests/cpp/prototype/workspace_registry_test.cpp
@@ -0,0 +1,36 @@
+#include "editor_proto/workspace_registry.hpp"
+
+#include <cassert>
+
+int main() {
+  using namespace editor_proto;
+
+  WorkspaceRegistry registry;
+
+  auto f1 = registry.open_file("/Repo/src/main.cpp");
+  assert(!f1.is_duplicate);
+  auto f2 = registry.open_file("\\repo\\src\\main.cpp");
+  assert(f2.is_duplicate);
+  assert(f2.actions.size() == 2);
+  assert(registry.is_file_open("/repo/src/main.cpp"));
+
+  auto d1 = registry.add_directory_root("/Work/ProjectA/");
+  assert(!d1.is_duplicate);
+  auto d2 = registry.add_directory_root("/work/projecta");
+  assert(d2.is_duplicate);
+  assert(registry.has_root("/WORK/projectA"));
+
+  assert(registry.open_file_count() == 1);
+  assert(registry.root_count() == 1);
+
+  ScrollController scroll;
+  auto wheel = scroll.wheel_scroll(10.0, 2.0);
+  assert(wheel.new_offset == 100.0);
+  auto pan = scroll.middle_button_pan(wheel.new_offset, -10.0);
+  assert(pan.new_offset == 60.0);
+
+  auto clamped = scroll.middle_button_pan(5.0, -100.0);
+  assert(clamped.new_offset == 0.0);
+
+  return 0;
+}
